f8e1b6aa990c0b6b41d6b9f9c23c4cde6ff60ca2
kt81 <ktsh81@gmail.com>
2012-12-18 12:32:51 +0900

    easy bugfix

--- setup.sh
+++ setup.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 # setup.
 [ ! -e ~/.zshrc ] && ln -s ~/.config/.zshrc ~/.zshrc
-[ ! -e ~/.zshrc ] && ln -s ~/.config/.vimrc ~/.vimrc
+[ ! -e ~/.vimrc ] && ln -s ~/.config/.vimrc ~/.vimrc
 touch ~/.zshrc.mine
