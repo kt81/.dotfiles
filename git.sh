a2b789f0071d230ca9f50c46a6a95a3beee98bff
kt81 <ktsh81@gmail.com>
2013-06-24 22:58:07 +0900

    カラー使用の定義を追加

--- git.sh
+++ git.sh
@@ -5,5 +5,6 @@ git config --global alias.co checkout
 git config --global alias.ci commit
 git config --global alias.st status
 git config --global alias.br branch
+git config --global color.ui auto
 git config --global alias.svn-diff '!f() { git svn dcommit -n | grep diff | awk -F'\''diff-tree'\'' -v opt=$@ '\''{print "git diff "opt" "$2}'\'' | sh; }; f'
 
