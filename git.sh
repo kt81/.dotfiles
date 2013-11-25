875f22515afa6b6f1d5d9a6526a1cfbb90621df7
kt81 <ktsh81@gmail.com>
2013-11-25 18:35:16 +0900

    define git push behavior

--- git.sh
+++ git.sh
@@ -6,7 +6,7 @@ git config --global alias.ci commit
 git config --global alias.st status
 git config --global alias.br branch
 git config --global color.ui auto
+git config --global push.default simple
 git config --global alias.svn-diff '!f() { git svn dcommit -n | grep diff | awk -F'\''diff-tree'\'' -v opt=$@ '\''{print "git diff "opt" "$2}'\'' | sh; }; f'
 git config --global alias.kfetch 'fetch --all --prune'
 git config --global alias.kup '!git fetch --all --prune && git checkout master && git merge --ff-only upstream/master && git push origin master'
-
