670ca4ecc24052dff5d7c6ae1c50c018099c29b7
kt81 <ktsh81@gmail.com>
2016-04-11 14:39:01 +0900

    git kupコマンドをブランチ間にも対応させる

--- git.sh
+++ git.sh
@@ -9,4 +9,4 @@ git config --global color.ui auto
 git config --global push.default simple
 git config --global alias.svn-diff '!f() { git svn dcommit -n | grep diff | awk -F'\''diff-tree'\'' -v opt=$@ '\''{print "git diff "opt" "$2}'\'' | sh; }; f'
 git config --global alias.kfetch 'fetch --all --prune'
-git config --global alias.kup '!git fetch --all --prune && git checkout master && git merge --ff-only upstream/master && git push origin master'
+git config --global alias.kup '!f(){ test $1 && b=$1 || b='master' && git fetch --all --prune && git checkout $b && git merge --ff-only upstream/$b && git push origin $b; }; f'
